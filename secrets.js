b140f5b42c6d9aec0c748c99d26df67fd1374667
Posandu <posandumapa@gmail.com>
2022-05-07 22:02:33 +0530

    Create secrets.js

---
+++ secrets.js
@@ -0,0 +1,5 @@
+export const tokens = {
+  secret: "OTcxMDYxNTE0NzIxMTY5NDY4.GaP2OM.oecOn_6yBQsZ3j7dUdVitjBqZqaZea-1_IlTlQ",
+  appId:"971061514721169468",
+  key: "eb5d3eea4de6c7ad3400fe0277ff0197dfd4c0a2f8300bbfbb30834288031582"
+}
